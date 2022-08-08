b5eeaa48867b8d6594cb69027121a2051a9a5bec
naimyasirozcan <naimyasirozcan@gmail.com>
2022-08-08 20:31:05 +0300

    local Storage info setted

--- js/app.js
+++ js/app.js
@@ -1,31 +1,45 @@
+
+
 let incrBtn = document.querySelector('#incrBtn')
 let decrBtn = document.querySelector('#decrBtn')
 let resBtn = document.querySelector('#resBtn')
-let result = document.querySelector('#result')
+let resultDOM = document.querySelector('#result')
+
+
+let result = localStorage.getItem('result') ? localStorage.getItem('result') : 0
+
+resultDOM.innerHTML = result
+
 
 incrBtn.addEventListener('click', function(){
-    let sayi = parseInt(result.innerHTML)
+    let sayi = parseInt(resultDOM.innerHTML)
 
     sayi += 1
 
-    result.innerHTML = `${sayi}`
+    localStorage.setItem('result', sayi)
+
+    resultDOM.innerHTML = `${sayi}`
 })
 
 decrBtn.addEventListener('click',function(){
-    let sayi = parseInt(result.innerHTML)
+    let sayi = parseInt(resultDOM.innerHTML)
 
     sayi -= 1
 
-    result.innerHTML = `${sayi}`
+    localStorage.setItem('result', sayi)
+
+    resultDOM.innerHTML = `${sayi}`
 }
 )
 
 resBtn.addEventListener('click',function(){
-    let sayi = parseInt(result.innerHTML)
+    let sayi = parseInt(resultDOM.innerHTML)
 
     sayi = 0
 
-    result.innerHTML = `${sayi}`
+    localStorage.setItem('result', sayi)
+
+    resultDOM.innerHTML = `${sayi}`
 }
 )
 
